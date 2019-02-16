8563fc5dc018a61affe32920aceeb8f90d7b7d5d
iradsanchez <ira.d.sanchez@gmail.com>
2019-02-16 11:25:36 -0700

    basic reducer implemented

---
+++ todo/src/reducers/index.js
@@ -0,0 +1,11 @@
+const initialState = {
+  todos: []
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+export default reducer;
