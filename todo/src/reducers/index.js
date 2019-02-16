2ed88315c093a7469f06c7f61f7d70f2695b9136
iradsanchez <ira.d.sanchez@gmail.com>
2019-02-16 11:29:39 -0700

    ADD_TODO case implemented in reducer

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -4,6 +4,18 @@ const initialState = {
 
 function reducer(state = initialState, action) {
   switch (action.type) {
+    case "ADD_TODO":
+      return {
+        todos: [
+          ...state,
+          {
+            id: action.id,
+            text: action.text,
+            completed: false
+          }
+        ]
+      };
+
     default:
       return state;
   }
