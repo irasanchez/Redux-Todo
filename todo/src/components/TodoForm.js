b70f38be9828e4234762e4c3ce9d2bdc2ee983f3
iradsanchez <ira.d.sanchez@gmail.com>
2019-02-16 11:58:25 -0700

    implemented TodoForm without connect

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,1 +1,31 @@
 import React from "react";
+import { addTodo } from "./../actions/";
+
+class TodoForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ""
+    };
+  }
+
+  handleChange = event => {
+    this.setState({ value: event.target.value.trim() });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addTodo(this.state);
+  };
+
+  render() {
+    return (
+      <form className="TodoForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="new task" value={this.state.value} />
+        <button type="submit">Add Task</button>
+      </form>
+    );
+  }
+}
+
+export default TodoForm;
